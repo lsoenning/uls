c27be4a9c01675a2863ee052c0093f706ef6b4b0
lsoenning <lukas.soenning@uni-bamberg.de>
2025-05-12 20:59:02 +0200

    minor fix to theme

--- R/theme_dotplot.R
+++ R/theme_dotplot.R
@@ -15,7 +15,7 @@ theme_dotplot <- function(vertical = FALSE){
         axis.text.x = ggplot2::element_blank(),
         axis.ticks.x = ggplot2::element_blank(),
         axis.title.x = ggplot2::element_blank(),
-        #strip.text.x.left = ggplot2::element_text(angle = 0, hjust=1, vjust=0),
+        strip.text.x.left = ggplot2::element_text(angle = 0, hjust=1, vjust=0),
         panel.grid = ggplot2::element_blank(),
         strip.text.x = ggplot2::element_text(angle=0, hjust=0, vjust=0))
   } else {
@@ -26,7 +26,7 @@ theme_dotplot <- function(vertical = FALSE){
         axis.text.y = ggplot2::element_blank(),
         axis.ticks.y = ggplot2::element_blank(),
         axis.title.y = ggplot2::element_blank(),
-        #strip.text.y.left = ggplot2::element_text(angle = 0, hjust=1, vjust=0),
+        strip.text.y.left = ggplot2::element_text(angle = 0, hjust=1, vjust=0),
         panel.grid = ggplot2::element_blank(),
         strip.text.y = ggplot2::element_text(angle=0, hjust=0, vjust=0))
   }
